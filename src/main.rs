aff22a3c72c8d706d6095b15e3a7d6717a696886
T.J. Yang <tjyang2001@gmail.com>
2024-02-04 17:20:02 -0600

    Automated commit message without details

--- src/main.rs
+++ src/main.rs
@@ -4,87 +4,102 @@ mod functions;
 mod hello;
 mod test;
 fn main() {
-    let matches = command!() // requires clap `cargo` feature in Cargo.toml
+    let matches = command!()
         .help_template(
             "{before-help}{name}-{version} {about-with-newline}{author-with-newline} 
 {usage-heading} [Options] [Commands] [Options] 
 
 {all-args}{after-help} ",
-        ) // requires clap `help` feature in Cargo.toml
+        )
         .version("1.1")
         .propagate_version(true)
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(
             Command::new("create")
-                .about("C-RUD: to create a file.\n Ex: hello create test.txt")
-                .arg(arg!([NAME])),
+                .about("Create operation")
+                .arg(arg!([CREATE_VALUE])),
         )
         .subcommand(
             Command::new("retrieve")
-                .about("C-R-UD: to retrieve a file")
+                .about("retrieve operation")
+                .arg(arg!([RETRIEVE_VALUE])),
+        )
+        .subcommand(
+            Command::new("update")
+                .about("Update operation")
+                .arg(arg!([UPDATE_VALUE])),
+        )
+        .subcommand(
+            Command::new("delete")
+                .about("Delete opteration")
                 .arg(arg!([NAME])),
         )
-        .subcommand(Command::new("update").about("CR-U-D ...").arg(arg!([NAME])))
-        .subcommand(Command::new("delete").about("CRU-D ...").arg(arg!([NAME])))
+        .subcommand(
+            Command::new("demo")
+                .about("funcions and classes demo")
+                .arg(arg!([DEMO_VALUE])),
+        )
         .get_matches();
 
     match matches.subcommand() {
-        Some(("create", sub_matches)) => println!(
-            "'myapp create' was used, name is: {:?}",
-            sub_matches.get_one::<String>("NAME")
-        ),
-        Some(("retrieve", sub_matches)) => println!(
-            "'myapp retrieve' was used, name is: {:?}",
-            sub_matches.get_one::<String>("NAME")
-        ),
-        Some(("update", sub_matches)) => println!(
-            "'myapp update' was used, name is: {:?}",
-            sub_matches.get_one::<String>("NAME")
-        ),
-        Some(("delete", sub_matches)) => {
+        Some(("create", sub_matches)) => {
             println!(
-                "'myapp delete' was used, name is: {:?}",
+                "'subcommand create' was used, argument is: {:?}",
                 sub_matches.get_one::<String>("NAME")
             );
-            functions::greeting("test".to_string());
-            functions::hello_world();
+            let result = functions::add(5, 3);
+            println!("functions::add return: {}", result);
         }
+        Some(("retrieve", sub_matches)) => {
+            println!(
+                "'subcommand retrieve' was used, argument is: {:?}",
+                sub_matches.get_one::<String>("NAME")
+            );
+            functions::greeting("Retrieve operation".to_string());
+            let result = functions::multiply(5, 3);
+            println!("functions::multiply return: {}", result);
+            let triangle = classes::Triangle {
+                base: 8.0,
+                height: 4.0,
+            };
 
+            println!(
+                "triangle of classes::Triangle instance retured: base = {}, height = {}, area = {}",
+                triangle.base,
+                triangle.height,
+                triangle.area()
+            );
+        }
+        Some(("update", sub_matches)) => {
+            println!(
+                "'subcommand update' was used, argument is: {:?}",
+                sub_matches.get_one::<String>("UPDATE_VALUE")
+            );
+            functions::greeting("Update operation".to_string());
+            let rectangle = classes::Rectangle {
+                width: 10.0,
+                height: 5.0,
+            };
+            println!("rectangle instance of classes::Rectangle return: width = {}, height = {}, area = {}",
+		     rectangle.width,
+		     rectangle.height,
+		     rectangle.area());
+        }
+        Some(("delete", sub_matches)) => {
+            println!(
+                "'subcommand delete' was used, argument is: {:?}",
+                sub_matches.get_one::<String>("NAME")
+            );
+            functions::greeting("Delete operation".to_string());
+            functions::hello_world();
+            let circle = classes::Circle { radius: 3.0 };
+            println!(
+                "circle instance of classes::Circle return: radius = {}, circumference = {}",
+                circle.radius,
+                circle.circumference()
+            );
+        }
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
-
-    hello::hello();
-    let result = functions::add(5, 3);
-    println!("functions::add return: {}", result);
-
-    let result = functions::multiply(5, 3);
-    println!("functions::multiply return: {}", result);
-
-    let rectangle = classes::Rectangle {
-        width: 10.0,
-        height: 5.0,
-    };
-    println!(
-        "rectangle instance of classes::Rectangle return: width = {}, height = {}, area = {}",
-        rectangle.width,
-        rectangle.height,
-        rectangle.area()
-    );
-    let circle = classes::Circle { radius: 3.0 };
-    println!(
-        "circle instance of classes::Circle return: radius = {}, circumference = {}",
-        circle.radius,
-        circle.circumference()
-    );
-    let triangle = classes::Triangle {
-        base: 8.0,
-        height: 4.0,
-    };
-    println!(
-        "triangle of classes::Triangle instance retured: base = {}, height = {}, area = {}",
-        triangle.base,
-        triangle.height,
-        triangle.area()
-    );
 }
