1dec040efb8008e87c82ad4b358dca9093e5caf2
T.J. Yang <tjyang2001@gmail.com>
2024-02-04 11:52:55 -0600

    Automated commit message without details

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod hello;
 mod test;
 // https://docs.rs/clap/4.4.18/clap/struct.Command.html#method.help_template
 // Valid tags are:
-// 
+//
 // {name} - Display name for the (sub-)command.
 // {bin} - Binary name.(deprecated)
 // {version} - Version number.
@@ -27,12 +27,13 @@ mod test;
 // {usage-heading} [Options] [Commands] [Options]
 
 fn main() {
-
     let matches = command!() // requires clap `cargo` feature in Cargo.toml
-        .help_template("{before-help}{name}-{version} {about-with-newline}{author-with-newline} 
+        .help_template(
+            "{before-help}{name}-{version} {about-with-newline}{author-with-newline} 
 {usage-heading} [Options] [Commands] [Options] 
 
-{all-args}{after-help} ")    // requires clap `help` feature in Cargo.toml
+{all-args}{after-help} ",
+        ) // requires clap `help` feature in Cargo.toml
         .version("1.1")
         .propagate_version(true)
         .subcommand_required(true)
@@ -47,16 +48,8 @@ fn main() {
                 .about("C-R-UD: to retrieve a file")
                 .arg(arg!([NAME])),
         )
-        .subcommand(
-            Command::new("update")
-                .about("CR-U-D ...")
-                .arg(arg!([NAME])),
-        )
-        .subcommand(
-            Command::new("delete")
-                .about("CRU-D ...")
-                .arg(arg!([NAME])),
-        )
+        .subcommand(Command::new("update").about("CR-U-D ...").arg(arg!([NAME])))
+        .subcommand(Command::new("delete").about("CRU-D ...").arg(arg!([NAME])))
         .get_matches();
 
     match matches.subcommand() {
@@ -72,14 +65,17 @@ fn main() {
             "'myapp update' was used, name is: {:?}",
             sub_matches.get_one::<String>("NAME")
         ),
-        Some(("delete", sub_matches)) => println!(
-            "'myapp delete' was used, name is: {:?}",
-            sub_matches.get_one::<String>("NAME")
-        ),
+        Some(("delete", sub_matches)) => {
+            println!(
+                "'myapp delete' was used, name is: {:?}",
+                sub_matches.get_one::<String>("NAME")
+            );
+            functions::greeting("test".to_string());
+            functions::hello_world();
+        }
 
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
-
 
     hello::hello();
     let result = functions::add(5, 3);
@@ -88,11 +84,30 @@ fn main() {
     let result = functions::multiply(5, 3);
     println!("Product: {}", result);
 
-    let rectangle = classes::Rectangle { width: 10.0, height: 5.0 };
+    let rectangle = classes::Rectangle {
+        width: 10.0,
+        height: 5.0,
+    };
     let circle = classes::Circle { radius: 3.0 };
-    let triangle = classes::Triangle { base: 8.0, height: 4.0 };
-    println!("Rectangle: width = {}, height = {}, area = {}", rectangle.width, rectangle.height, rectangle.area());
-    println!("Circle: radius = {}, circumference = {}", circle.radius, circle.circumference());
-    println!("Triangle: base = {}, height = {}, area = {}", triangle.base, triangle.height, triangle.area());
+    let triangle = classes::Triangle {
+        base: 8.0,
+        height: 4.0,
+    };
+    println!(
+        "Rectangle: width = {}, height = {}, area = {}",
+        rectangle.width,
+        rectangle.height,
+        rectangle.area()
+    );
+    println!(
+        "Circle: radius = {}, circumference = {}",
+        circle.radius,
+        circle.circumference()
+    );
+    println!(
+        "Triangle: base = {}, height = {}, area = {}",
+        triangle.base,
+        triangle.height,
+        triangle.area()
+    );
 }
-
--- src/test.rs
+++ src/test.rs
@@ -1,26 +1,29 @@
 fn greeting(name: String) -> String {
-   let hello = String::from("Hello, ");
+    let hello = String::from("Hello, ");
     let greeting = format!("{hello}{name}!");
     greeting
 }
 
 fn hello_world() -> String {
-    let greeting = String::from("Hello, World!");
-    greeting
+    String::from("Hello, World!")
 }
 
+// #[test]
+#[cfg(test)]
 
-#[test]
-fn hello_world_test() {
-    let want = String::from("Hello, World!");
-    let result = hello_world();
-    assert_eq!(want, result);
+mod hellocrud_tests {
+    use super::*;
+    #[test]
+    fn hello_world_test() {
+        let want = String::from("Hello, World!");
+        let result = hello_world();
+        assert_eq!(want, result);
+    }
+    #[test]
+    fn greeting_test() {
+        let want = String::from("Hello, Rusty!");
+        let name = String::from("Rusty");
+        let result = greeting(name);
+        assert_eq!(want, result);
+    }
 }
-#[test]
-fn greeting_test() {
-    let want = String::from("Hello, Rusty!");
-    let name = String::from("Rusty");
-    let result = greeting(name);
-    assert_eq!(want, result);
-}
-
