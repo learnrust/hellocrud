69e631b20717efd2d322429d9ffc15e3447e865e
TJ Yang <tjyang2001@gmail.com>
2024-02-04 09:40:59 -0600

    Automated commit message without details

---
+++ src/functions.rs
@@ -0,0 +1,20 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    let sum = a + b;
+    sum
+}
+
+pub fn multiply(a: i32, b: i32) -> i32 {
+    let product = a * b;
+    product
+}
+
+//pub fn greeting(name: String) -> String {
+//   let hello = String::from("Hello, ");
+//    let greeting = format!("{hello}{name}!");
+//    greeting
+//}
+//
+//pub fn hello_world() -> String {
+//    let greeting = String::from("Hello, World!");
+//    greeting
+//}
